2487790d76b479588de1a3a87de874c8e52f0a5a
Nicholas Rejack <nrejack@gmail.com>
2019-07-19 10:53:30 -0400

    Adding two new scripts that toggle flags for noout, etc cluster-wide.

---
+++ tools/flags_after.sh
@@ -0,0 +1,4 @@
+ceph osd unset noout
+ceph osd unset nobackfill
+ceph osd unset norecover
+ceph osd unset norebalance
---
+++ tools/flags_before.sh
@@ -0,0 +1,4 @@
+ceph osd set noout
+ceph osd set norebalance
+ceph osd set norecover
+ceph osd set nobackfill
